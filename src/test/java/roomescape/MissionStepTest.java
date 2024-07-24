7c20417edb8667046267d294b6b7a30b124cab89
coli-geonwoo <coli-geonwoo@gmail.com>
2024-07-24 19:46:42 +0900

    fix : 실패 테스트 개선

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
     void test() {
-        throw new RuntimeException();
     }
 
 }
